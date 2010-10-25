7d70ec8c80e4995b1c14a09fe55b56a5bbc2eeb6
Bruno Garcia <bg05he@brocku.ca>
2010-10-25 02:33:59 -0700

    Simple KISS metrics integration.

---
+++ src/kissmetrics.js
@@ -0,0 +1,19 @@
+goog.provide("hydra.kissmetrics");
+
+/**
+ * @param {string} apiKey
+ */
+hydra.kissmetrics.init = function (apiKey) {
+    if (navigator.onLine !== false) {
+        window["_kmq"] = [];
+        var _kmq = _kmq || [];
+        var load = function (src) {
+            var script = document.createElement("script");
+            script.src = src;
+            script.async = true;
+            document.body.appendChild(script);
+        }
+        load("http://i.kissmetrics.com/i.js");
+        load("http://doug1izaerwt3.cloudfront.net/" + apiKey + ".1.js");
+    }
+}
