7270cacae5944bc3cf2f1d6c290abf6cb064c527
Bruno Garcia <bg05he@brocku.ca>
2011-01-08 23:11:13 -0800

    Refined admob integration.

--- src/api/admob.js
+++ src/api/admob.js
@@ -1,32 +1,34 @@
-goog.provide("hydra.admob");
+goog.provide("hydra.api.admob");
 
-/** @define {string} */
-hydra.admob.PUBLISHER_ID = "a14c13755c46172";
-
-hydra.admob.showAd = function (element) {
-    if (_admob) {
+/**
+ * @param {Element} element
+ */
+hydra.api.admob.showAd = function (element) {
+    if (element && window["_admob"]) {
+        element.textContent = ""; // Clear it out first
         _admob.fetchAd(element);
     }
 }
 
 /**
  * @param {string} publisherId
- * @param {HTMLElement} element
+ * @param {Element} element
  */
-hydra.admob.init = function (publisherId, element) {
-    if (navigator.onLine) {
-        admob_vars = {
+hydra.api.admob.init = function (publisherId, element) {
+    if (navigator.onLine !== false) {
+        window["admob_vars"] = {
             "pubid": publisherId,
             "bgcolor": "000000", // background color (hex)
             "text": "FFFFFF", // font-color (hex)
             "ama": false, // set to true and retain comma for the AdMob Adaptive Ad Unit, a special ad type designed for PC sites accessed from the iPhone.  More info: http://developer.admob.com/wiki/IPhone#Web_Integration
             "test": goog.DEBUG, // test mode, set to false to receive live ads
-            "manual_mode": true
+            "manual_mode": true,
+            "new_window": true
         };
         var script = document.createElement("script");
         script.src = "http://mmv.admob.com/static/iphone/iadmob.js";
         script.onload = function () {
-            _admob.fetchAd(element);
+            hydra.api.admob.showAd(element);
         }
         document.body.appendChild(script);
     }
