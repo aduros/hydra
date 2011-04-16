435ec265775a10a16795340a461baebf31f9acdb
Bruno Garcia <ah.duros@gmail.com>
2011-04-16 03:16:25 -0700

    Added Zeewe API.

---
+++ src/api/zeewe.js
@@ -0,0 +1,22 @@
+goog.provide("hydra.api.zeewe");
+
+goog.require("goog.Uri");
+
+hydra.api.zeewe.getUserId = function () {
+    return goog.Uri.parse(document.location.href).getParameterValue("zeewe_user");
+}
+
+/**
+ * @param {number} score
+ */
+hydra.api.zeewe.redirectWithScore = function (score) {
+    document.location = "gameover?zeewe_user=" +
+        hydra.api.zeewe.getUserId() + "&score=" + score;
+}
+
+/**
+ * Whether this is a redirect back from Zeewe.
+ */
+hydra.api.zeewe.isReturning = function () {
+    return goog.Uri.parse(document.location.href).getParameterValue("scene") == "playing";
+}
