9904037bbb780e077d194eb67e30b5b9aa6fd35d
Bruno Garcia <ah.duros@gmail.com>
2011-06-12 16:45:22 -0700

    SlideTransition fix for large screens (tablets, desktop).

--- src/transition.js
+++ src/transition.js
@@ -2,6 +2,8 @@ goog.provide("hydra.Transition");
 goog.provide("hydra.SlideTransition");
 goog.provide("hydra.FadeTransition");
 
+goog.require("hydra.director");
+
 /**
  * @constructor
  * @extends {hydra.Scene}
@@ -41,12 +43,14 @@ goog.inherits(hydra.SlideTransition, hydra.Transition);
 hydra.SlideTransition.prototype.load = function () {
     goog.base(this, "load");
 
-    this.nextScene.getRoot().setX(320); // TODO: Support larger resolutions
+    var width = hydra.director.getStage().clientWidth;
+    this.nextScene.getRoot().setX(width);
+
     var self = this;
     this.addTask(new hydra.task.Sequence([
         new hydra.task.Parallel([
             // TODO: Support non-linear transitions
-            hydra.task.MoveTo.linear(-320, 0, this.duration, hydra.director.getPreviousScene().getRoot()),
+            hydra.task.MoveTo.linear(-width, 0, this.duration, hydra.director.getPreviousScene().getRoot()),
             hydra.task.MoveTo.linear(0, 0, this.duration, this.nextScene.getRoot())
         ]),
         new hydra.task.CallFunction(function () {
